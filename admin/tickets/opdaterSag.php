2a3accba961bf48f10d5abfb0e0602a206d8036e
Kaki-the-Kid <karsten.reitan@gmail.com>
2021-11-26 09:27:19 +0100

    Erstattet kode med footer kald

--- admin/tickets/opdaterSag.php
+++ admin/tickets/opdaterSag.php
@@ -154,19 +154,5 @@ $userdata = $user->getInfo(Session::get('userID'));
                                 </div>
                             </div>
 
-                            <footer class="text-center">© Aarhus Tech SKP 2018-2021</footer>
-                            <footer class="text-center">Udviklet af elever og instruktører SKP Data IT</footer>
-                            <footer class="text-center">Version <?php echo Config::get('system_info/version'); ?></footer>
-                           </blockquote>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <script src="/assets/js/jquery.min.js"></script>
-        <script src="/assets/js/popper.js"></script>
-        <script src="/assets/js/bootstrap.min.js"></script>
-        <script src="/assets/js/dismiss-alerts.js"></script>
-    </body>
-</html>
+include_once "../../assets/tpl/cases_footer.php";
+?>
