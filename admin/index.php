8f58c87196d2cf4f25511d617ebaa1f1a64be0df
Kaki-the-Kid <karsten.reitan@gmail.com>
2022-04-26 09:49:13 +0200

    Cleaning op code

--- admin/index.php
+++ admin/index.php
@@ -10,19 +10,18 @@ if(!Session::exists('userID')) {
 }
 
 
-
 $userdata = $user->getInfo(Session::get('userID'));
 
 require_once("../assets/tpl/splash_header.php");
 
 if ( $user->isAdmin(Session::get('userID')) ) 
 {
- ?>
+?>
                                     <div class="form-group text-center">
                                         <a class="btn btn-outline-success full-width" href="/admin/bruger/">Bruger systemet</a>
                                     </div>
 <?php 
 }
-	require_once("../assets/tpl/splash_footer.php"); 
+	
+require_once("../assets/tpl/splash_footer.php"); 
 ?>
-
